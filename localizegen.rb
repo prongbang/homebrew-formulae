ddeb00ec80b8573187e772142214382480336703
prongbang <prongbang@gmail.com>
2021-09-18 01:31:32 +0700

    Update formula script

--- localizegen.rb
+++ localizegen.rb
@@ -1,9 +1,9 @@
 class Localizegen < Formula
-    desc "Generate Localization tools for Mobile Application an Android, iOS"
-    homepage "https://github.com/prongbang/localizegen"
-    url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.2.tar.gz"
-    sha256 "0e32a9d27a65781930370d5c162f15bfa42d03dea5eb1f15bd9b532542c50b68"
-    license "MIT"
+	desc "Generate Localization tools for Mobile Application an Android, iOS"
+	homepage "https://github.com/prongbang/localizegen"
+	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.2.tar.gz"
+	sha256 "0e32a9d27a65781930370d5c162f15bfa42d03dea5eb1f15bd9b532542c50b68"
+	license "MIT"
 
 	bottle :unneeded
 
