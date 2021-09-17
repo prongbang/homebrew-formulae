aef37147e28790ef775b40c9bb4884e56f23eb20
prongbang <prongbang@gmail.com>
2021-09-18 01:01:11 +0700

    add localizegen.rb formula

---
+++ README.md
@@ -0,0 +1,13 @@
+# homebrew-formulae
+
+## Localizegen
+
+- Generate formula
+
+```shell
+brew create https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.2.tar.gz
+```
+
+Output
+
+- [localizegen.rb](https://github.com/prongbang/homebrew-formulae/localizegen.rb)
---
+++ localizegen.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Localizegen < Formula
+  desc "Generate Localization tools for Mobile Application an Android, iOS"
+  homepage ""
+  url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "0e32a9d27a65781930370d5c162f15bfa42d03dea5eb1f15bd9b532542c50b68"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test localizegen`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
