dc5b168fd413d0889d8218872cc12755cbb2ecea
prongbang <prongbang@gmail.com>
2021-09-18 01:20:14 +0700

    Update formula script

--- localizegen.rb
+++ localizegen.rb
@@ -1,14 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Localizegen < Formula
   desc "Generate Localization tools for Mobile Application an Android, iOS"
   homepage "https://github.com/prongbang/localizegen"
   url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "0e32a9d27a65781930370d5c162f15bfa42d03dea5eb1f15bd9b532542c50b68"
   license "MIT"
-
-  # depends_on "cmake" => :build
 
   bottle :unneeded
 
