117f908d32744c2a2fa0106f67352591c9998061
prongbang <prongbang@gmail.com>
2021-10-16 22:04:28 +0700

    Update new version to 1.0.8

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Generate Formula
 
 ```shell
-$ brew create https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.7.tar.gz
+$ brew create https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.8.tar.gz
 ```
 
 Please run `brew audit --new localizegen` before submitting, thanks.
--- localizegen.rb
+++ localizegen.rb
@@ -1,8 +1,8 @@
 class Localizegen < Formula
 	desc "Generate Localization tools for Mobile Application an Android, iOS"
 	homepage "https://github.com/prongbang/localizegen"
-	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.7.tar.gz"
-	sha256 "f75eafc84a38c23953b098489456e14a3afc4160dd31090164612e8246a9f594"
+	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.8.tar.gz"
+	sha256 "cea82d58e0b74c090a156aba437a95c44aae50f991eaa3e902327a6e83127d6a"
 	license "MIT"
 
 	bottle :unneeded
