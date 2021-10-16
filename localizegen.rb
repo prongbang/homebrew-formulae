0369c477a764b423f9307b031874708d3f874e51
prongbang <prongbang@gmail.com>
2021-10-16 22:47:58 +0700

    Update new version to 1.0.9

--- localizegen.rb
+++ localizegen.rb
@@ -1,8 +1,8 @@
 class Localizegen < Formula
 	desc "Generate Localization tools for Mobile Application an Android, iOS"
 	homepage "https://github.com/prongbang/localizegen"
-	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.8.tar.gz"
-	sha256 "cea82d58e0b74c090a156aba437a95c44aae50f991eaa3e902327a6e83127d6a"
+	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.0.9.tar.gz"
+	sha256 "bf526886547d5e68beec2048acdaa27506aa3102f1ab310e266d5c2f5aa1de0b"
 	license "MIT"
 
 	bottle :unneeded
