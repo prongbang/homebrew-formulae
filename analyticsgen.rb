0e6bb05c3379925e83f07b84a59241007e8c8b5d
prongbang <prongbang@gmail.com>
2021-12-08 13:15:24 +0700

    Update checksum

--- analyticsgen.rb
+++ analyticsgen.rb
@@ -2,7 +2,7 @@ class Analyticsgen < Formula
 	desc "Google Sheet Analytics Generator supported Flutter, Android, and iOS"
 	homepage "https://github.com/prongbang/analyticsgen"
 	url "https://github.com/prongbang/analyticsgen/archive/refs/tags/v1.0.2.tar.gz"
-	sha256 "6739b2b431b3b3195e4f79e6d236a58011414162bfc640e782735622ddc71f14"
+	sha256 "86a7fe2305ef75629cf92702ac4b7ed161cf2c7dbc2c2c83b0ead59289a4c6eb"
 	license "MIT"
 
 	bottle :unneeded
