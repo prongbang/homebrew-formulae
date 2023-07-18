72b94ca420329e6dd1bdafdbcbf1a4f21cd587ec
prongbang <prongbang@gmail.com>
2023-07-18 14:03:38 +0700

    Update localizegen new version 1.1.5

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Generate Formula
 
 ```shell
-$ brew create https://github.com/prongbang/localizegen/archive/refs/tags/v1.1.4.tar.gz
+$ brew create https://github.com/prongbang/localizegen/archive/refs/tags/v1.1.5.tar.gz
 ```
 
 Please run `brew audit --new localizegen` before submitting, thanks.
--- localizegen.rb
+++ localizegen.rb
@@ -1,12 +1,22 @@
 class Localizegen < Formula
 	desc "Generate Localization tools for Mobile Application an Android, iOS"
 	homepage "https://github.com/prongbang/localizegen"
-	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.1.4.tar.gz"
-	sha256 "4d4561a10b6556aa3e7132ab4424dd50df6e782b9ecbee7c79da14b0cf3aea76"
+	url "https://github.com/prongbang/localizegen/archive/refs/tags/v1.1.5.tar.gz"
+	sha256 "738cbc1625df983309ba24c1fcd38a60e6051741f0635be38fbb908f2dbeb7d3"
 	license "MIT"
 
 	def install
-		bin.install "localizegen"
+		on_macos do
+          bin.install "localizegen"
+        end
+
+        on_linux do
+          bin.install "binary/linux/localizegen"
+        end
+
+        on_windows do
+          bin.install "binary/windows/localizegen"
+        end
 	end
 
 	test do
