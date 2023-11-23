9b2060f7b2942049c301116b5e7e749bfab35837
prongbang <prongbang@gmail.com>
2023-11-23 11:28:02 +0700

    config: new version analyticsgen

--- analyticsgen.rb
+++ analyticsgen.rb
@@ -1,33 +1,25 @@
 class Analyticsgen < Formula
-	desc "Google Sheet Analytics Generator supported Flutter, Android, and iOS"
-	homepage "https://github.com/prongbang/analyticsgen"
-  	url "https://github.com/prongbang/analyticsgen/archive/refs/tags/v1.0.6.tar.gz"
-  	sha256 "94906f51d42949a28b35d84241449851dec3ddb9598cc1273ebbc7d908eca0fc"
-	license "MIT"
+  desc "Google Sheet Analytics Generator supported Flutter, Android, and iOS"
+  homepage "https://github.com/prongbang/analyticsgen"
+  url "https://github.com/prongbang/analyticsgen/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "94906f51d42949a28b35d84241449851dec3ddb9598cc1273ebbc7d908eca0fc"
+  license "MIT"
 
-	def install
-		bin.install "bin/macos/analyticsgen"
-	end
-
-  on_macos do
-    def install
-        bin.install "bin/macos/analyticsgen"
+  def install
+    on_macos do
+      bin.install "bin/macos/analyticsgen"
     end
-  end
 
-  on_linux do
-    def install
+    on_linux do
       bin.install "bin/linux/analyticsgen"
     end
-  end
 
-  on_window do
-    def install
+    on_windows do
       bin.install "bin/windows/analyticsgen.exe"
     end
   end
 
-	test do
-		system "#{bin}/analyticsgen"
-	end
+  test do
+    system "#{bin}/analyticsgen"
+  end
 end
