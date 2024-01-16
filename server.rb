5e27a27a0a162bc2965494532408105ff6022918
prongbang <prongbang@gmail.com>
2024-01-16 23:58:18 +0700

    config: update server.rb

--- server.rb
+++ server.rb
@@ -1,24 +1,24 @@
 class Server < Formula
   desc "Rust-based HTTP server, serves any directory, inspired by python -m http.server."
-  homepage "https://github.com/prongbang/serve"
+  homepage "https://github.com/prongbang/server"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prongbang/serve/releases/download/0.1.0/0.1.0_Darwin_x86_64.tar.gz"
+      url "https://github.com/prongbang/server/releases/download/0.1.0/0.1.0_Darwin_x86_64.tar.gz"
       sha256 "235da825baa2000b29cf4e8e243f6c270c3c056a3f3838732b2f5b2ff784a3a4"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/prongbang/serve/releases/download/0.1.0/0.1.0_Darwin_arm64.tar.gz"
+      url "https://github.com/prongbang/server/releases/download/0.1.0/0.1.0_Darwin_arm64.tar.gz"
       sha256 "856f795116e15ec692eb8d322917ac301a7faa4e1f7d79edde32a08ef24d566f"
     end
 
     def install
-      bin.install "serve"
+      bin.install "server"
     end
   end
 
   test do
-    system "#{bin}/serve"
+    system "#{bin}/server"
   end
 end
