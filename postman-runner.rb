d9cb4644977bb4abf450337fe0ac69baa56007d0
prongbang <prongbang@gmail.com>
2024-01-24 18:47:23 +0700

    config: update postman-runner.rb version 0.3.0

--- postman-runner.rb
+++ postman-runner.rb
@@ -5,16 +5,16 @@ class PostmanRunner < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prongbang/postman-runner/releases/download/0.2.0/0.2.0_Darwin_x86_64.tar.gz"
-      sha256 "d91a80abf411e1889835ddb6a8cb46b7e2b77830babe1c3c42f5b2aff75a24ff"
+      url "https://github.com/prongbang/postman-runner/releases/download/0.3.0/0.3.0_Darwin_x86_64.tar.gz"
+      sha256 "97ba150ecb07ea07e2798f98580461ff319f633e9294627aaf1dc1e5ca38540b"
 
       def install
         bin.install "postman-runner"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/prongbang/postman-runner/releases/download/0.2.0/0.2.0_Darwin_arm64.tar.gz"
-      sha256 "1d02f764592e235dbcde6439d310ef21a869f6389b55a242298fc80242145dbf"
+      url "https://github.com/prongbang/postman-runner/releases/download/0.3.0/0.3.0_Darwin_arm64.tar.gz"
+      sha256 "e7f7d619f35c611c60faf8af904d0fc942087392207ecce6c87a32e6bbe24f34"
 
       def install
         bin.install "postman-runner"
