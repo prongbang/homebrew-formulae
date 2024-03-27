69c95c74c07893ba7c88c2cff369eb912f309024
prongbang <prongbang@gmail.com>
2024-03-27 23:14:11 +0700

    config: change herpy checksum

--- herpy.rb
+++ herpy.rb
@@ -6,11 +6,11 @@ class Herpy < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/prongbang/herpy/releases/download/0.1.0/0.1.0_Darwin_x86_64.tar.gz"
-      sha256 "09191d0b2b84dcd6fb9866deb73abc9d86278f1835a5f12be608073016f20a6b"
+      sha256 "5ad91d4170b381b1cd4149d53a7c6f8a7ba3df2473fe204f1a8ee757343e8232"
     end
     if Hardware::CPU.arm?
       url "https://github.com/prongbang/herpy/releases/download/0.1.0/0.1.0_Darwin_arm64.tar.gz"
-      sha256 "8d3426fb4341059a8808e1c12dd20ea82c589692b93673b7e12477c463c63877"
+      sha256 "a03496b0ce61c910f97583bee9f289281e654bc47882289e644e0e47733794ae"
     end
 
     def install
