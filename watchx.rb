8bd4c8ddc282e0c16097cb2a40ff487f99e88502
prongbang <prongbang@gmail.com>
2025-04-07 19:14:59 +0700

    feat: new version watchx

--- watchx.rb
+++ watchx.rb
@@ -5,12 +5,12 @@ class Watchx < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prongbang/watchx/releases/download/0.2.0/0.2.0_Darwin_x86_64.tar.gz"
-      sha256 "412ebfc67f8d4efe88ccacabe0bd3bca5d83f8af396de59d070c6e9ff71f27d2"
+      url "https://github.com/prongbang/watchx/releases/download/0.2.1/0.2.1_Darwin_x86_64.tar.gz"
+      sha256 "d91904b56be12223812b70859ce421b988dbb79262369352e8cd9d7fb5011e5a"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/prongbang/watchx/releases/download/0.2.0/0.2.0_Darwin_arm64.tar.gz"
-      sha256 "fc04e483f9bd6cc9d6abe42b4869cf7dacdeaca10483865bb84ff0940b2951fe"
+      url "https://github.com/prongbang/watchx/releases/download/0.2.1/0.2.1_Darwin_arm64.tar.gz"
+      sha256 "e795ed783c8bc6dd658e5832c26ff1804ff181c07605bda633f6bec64ffd18a5"
     end
 
     def install
